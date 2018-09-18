051f3cc2ff87c872e60c1ef3faa6ec0f5de1b7d0
Kaspern95 <34809965+Kaspern95@users.noreply.github.com>
2018-09-18 14:33:51 +0200

    PC files
    pc files

---
+++ Stuff/Stuff/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Stuff
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Title = "Things";
+            Console.WindowWidth = 100;
+            Console.WindowHeight = 20;
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+
+            Console.WriteLine("Please input your height in cm:");
+            string getHeight = Console.ReadLine();
+            double newHeight = double.Parse(getHeight);
+            double height = Math.Pow(newHeight, 2);
+            Console.WriteLine($"Your height is {newHeight}cm.");
+            
+            
+           
+
+
+
+           
+
+            Console.ReadLine();
+
+        }
+         
+    }
+}
